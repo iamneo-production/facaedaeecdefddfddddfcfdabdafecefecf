c2fa2994e68a08f5bab382866888733c344969f8
Iamneo <techadmin@iamneo.ai>
2023-08-05 14:28:41 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,9 +1,13 @@
+package com.examly.springapp;
+
+import org.testng.annotations.Test;
 import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
-import org.testng.Assert;
+
 import org.testng.annotations.AfterTest;
 
 public class SpringappApplication {
