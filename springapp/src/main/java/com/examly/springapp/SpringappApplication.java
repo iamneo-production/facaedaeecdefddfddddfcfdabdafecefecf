79a8da0eadf6988608033be22068a1b186403ebc
Iamneo <techadmin@iamneo.ai>
2023-08-05 11:23:30 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,13 +1,69 @@
 package com.examly.springapp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.testng.annotations.Test;
+import java.net.URL;
 
-@SpringBootApplication
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
 public class SpringappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringappApplication.class, args);
+	ChromeOptions chromeOptions = new ChromeOptions();
+	WebDriver driver = null;
+
+	@BeforeTest
+	public void beforeTest() throws Exception
+	 {
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+	    driver.manage().window().maximize();
+	}
+
+	@Test
+//Checking the title of iamNeo (Home - iamneo)
+	public void iamNeo() throws InterruptedException 
+	{
+        driver.navigate().to("http://iamneo.ai");
+		 String title =driver.getTitle();
+		Assert.assertEquals(title, "Learning and assessment solution for Universities and Enterprises");
+	}
+	@Test
+//Moving to FACEBOOK
+	public void nextPage() throws InterruptedException 
+	{
+	    driver.navigate().to("https://www.facebook.com");
+		 String title =driver.getTitle();
+		// Assert.assertEquals(title, "Facebook â€“ log in or sign up");
+
+	}
+	@Test
+//Back to iamNeo
+	public void backPage() throws InterruptedException 
+	{
+		driver.navigate().back();
+		String title =driver.getTitle();
+		// Assert.assertEquals(title, "Learning and assessment solution for Universities and Enterprises");
+
+	}
+	@Test
+//Current URL
+public void currentURL() throws InterruptedException 
+{
+		 String title =driver.getCurrentUrl();
+		 System.out.println(title);
+		// Assert.assertEquals(title, "");
+		driver.navigate().forward();
+		driver.navigate().refresh();
+
+}
+
+	@AfterTest
+	public void afterTest() 
+	{
+		driver.quit();
 	}
 
 }
